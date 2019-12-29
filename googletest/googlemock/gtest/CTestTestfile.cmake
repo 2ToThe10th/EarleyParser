d92b88e222a20f76962acb7d40cdc87a1287fad9
2ToThe10th <grib.a@phystech.edu>
2019-12-29 03:21:34 +0200

    googletest added

---
+++ googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sasha/TP/Game/googletest-master/googletest
+# Build directory: /home/sasha/TP/Game/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
